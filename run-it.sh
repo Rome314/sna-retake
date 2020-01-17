783a6febfb275692c44fe839466db28ca66815cc
Rome314 <401206176@mail.ru>
2020-01-17 06:08:46 +0300

    .

--- run-it.sh
+++ run-it.sh
@@ -3,6 +3,6 @@
 sudo apt-get update;
 sudo apt-get install -y docker.io;
 sudo apt-get install -y docker-compose;
-echo "*.* @127.0.0.1:5140" >> /etc/rsyslog.conf;
+sudo echo "*.* @127.0.0.1:5140" >> /etc/rsyslog.conf;
 sudo service rsyslog restart;
 sudo docker-compose up -d;
